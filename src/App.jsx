7cc341da845f3216cb0b8b6ced7589a1a5fe874d
Sakib Fakir <fakirsakib22232>
2025-02-01 23:42:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <div>
+      <h2 className='text-4xl font-bold'>Hello my project </h2>
+      <button className='btn btn-primary'>hello</button>
+      
+    </div>
+  )
+}
+
+export default App
