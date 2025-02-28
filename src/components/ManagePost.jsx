641d6f9e6f83be817f8e2e904c99deedb33a85d3
Sakib Fakir <FakirSakib22232@gmail.com>
2025-02-28 23:13:55 +0600

    delete done

--- src/components/ManagePost.jsx
+++ src/components/ManagePost.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
 import useAuthMangedHook from "../hook/useAuthMangedHook";
 import usePublicHook, { useaxiosPublic } from "../Api/usePublicHook";
+import Swal from 'sweetalert2'
 
-import { useQuery } from "@tanstack/react-query";
+
+
+import {
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { toast } from "react-toastify";
 function ManagePost() {
   // show post user email
   /// add edit , delete option
@@ -11,6 +20,7 @@ function ManagePost() {
 
   const { user } = useAuthMangedHook();
   const useaxiosapi = usePublicHook();
+  const queryclient = useQueryClient();
 
   const { data: myPost = [], isLoading } = useQuery({
     queryKey: ["mypost", user?.email],
@@ -24,10 +34,33 @@ function ManagePost() {
   console.log(myPost);
 
 
-  const handelDelete = (id)=>{
+  const deleteButton = async(id)=>{
 
+   
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async(result) => {
+
+      if (result.isConfirmed) {
+
+       try{
+        await useaxiosapi.delete(`/manage-post-delete/${id}`)
+        toast.success("Deleted")
+       }catch(error)
+       {
+        toast.error(error.message)
+       }
+
+      }
+    });
     
-
+    
   }
 
 
@@ -45,7 +78,6 @@ function ManagePost() {
       ) : (
         <section>
           {myPost.map((item, key) => (
-
             <div className="border flex justify-center m-2" key={key}>
               <div className="">
                 <img src={item?.Image} className="h-48" />
@@ -53,29 +85,23 @@ function ManagePost() {
                 <div>
                   <p>{item.Title}</p>
                   <p>{item.Message}</p>
-                  <div className="flex border gap-2"> 
+                  <div className="flex border gap-2">
                     <p>Like :{item.Like}</p>
-                    <p>UnLike :{ item.UnLike}</p>
+                    <p>UnLike :{item.UnLike}</p>
                     <p>Follow Post : {item.FollowPost}</p>
-
-
-
-
                   </div>
 
                   {/* button */}
-                 
-                 <div className="flex justify-between">
 
-                 <button className="btn btn-primary">Edit</button>
-                 <button className="btn btn-warning">Delete</button>
-                 </div>
-
-
-
-
-
-
+                  <div className="flex justify-between">
+                    <button className="btn btn-primary">Edit</button>
+                    <button
+                      className="btn btn-warning"
+                      onClick={() => deleteButton(item._id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
