b326da1b77e245f1195a99485e9be4c23a1d1dcc
Sakib Fakir <FakirSakib22232@gmail.com>
2025-02-28 22:20:06 +0600

    api fetch done

--- src/components/ManagePost.jsx
+++ src/components/ManagePost.jsx
@@ -1,48 +1,89 @@
+import React, { useEffect, useState } from "react";
+import useAuthMangedHook from "../hook/useAuthMangedHook";
+import usePublicHook, { useaxiosPublic } from "../Api/usePublicHook";
 
-
-
-import React, { useEffect, useState } from 'react'
-import useAuthMangedHook from '../hook/useAuthMangedHook'
-import { useaxiosPublic } from '../Api/usePublicHook';
-
+import { useQuery } from "@tanstack/react-query";
 function ManagePost() {
+  // show post user email
+  /// add edit , delete option
 
-  // show post user email 
-  /// add edit , delete option 
+  // fetures side bar
 
-  // fetures side bar 
-  const [ data , setdata ] =useState([]);
-  const {user , loading  } = useAuthMangedHook();
-  const useaxiosapi = useaxiosPublic();
+  const { user } = useAuthMangedHook();
+  const useaxiosapi = usePublicHook();
 
+  const { data: myPost = [], isLoading } = useQuery({
+    queryKey: ["mypost", user?.email],
 
-  useEffect(()=>{
-
-    useaxiosPublic.get(`/mange-post/${user?.email}`)
-    .then((res)=>{
-      setdata(res?.data)
-      console.log(res);
-    })
-    .catch((error)=>{
-      console.log(error.message)
-    })
-
-  },[])
+    queryFn: async () => {
+      const res = await useaxiosapi.get(`/manage-post/${user?.email}`);
+      return res.data;
+    },
+    enabled: !!user?.email,
+  });
+  console.log(myPost);
 
 
+  const handelDelete = (id)=>{
+
+    
+
+  }
 
 
 
   return (
-    <div>
-      <p>manage post</p>
-      
+    <div className="border">
+      <p>manage post {myPost?.length}</p>
 
-      
+      {/* section take contet */}
+
+      {isLoading ? (
+        <div className="py-16 border">
+          <span className="loading loading-ring  w-32"></span>
+        </div>
+      ) : (
+        <section>
+          {myPost.map((item, key) => (
+
+            <div className="border flex justify-center m-2" key={key}>
+              <div className="">
+                <img src={item?.Image} className="h-48" />
+
+                <div>
+                  <p>{item.Title}</p>
+                  <p>{item.Message}</p>
+                  <div className="flex border gap-2"> 
+                    <p>Like :{item.Like}</p>
+                    <p>UnLike :{ item.UnLike}</p>
+                    <p>Follow Post : {item.FollowPost}</p>
 
 
+
+
+                  </div>
+
+                  {/* button */}
+                 
+                 <div className="flex justify-between">
+
+                 <button className="btn btn-primary">Edit</button>
+                 <button className="btn btn-warning">Delete</button>
+                 </div>
+
+
+
+
+
+
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
+      )}
     </div>
-  )
+  );
 }
 
-export default ManagePost
+export default ManagePost;
