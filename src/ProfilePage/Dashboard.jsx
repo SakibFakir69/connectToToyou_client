3c57f072ffc67e723a1f635fe0964dd8927b710d
Sakib Fakir <FakirSakib22232@gmail.com>
2025-03-07 01:45:24 +0600

    dashboard half complete

--- src/ProfilePage/Dashboard.jsx
+++ src/ProfilePage/Dashboard.jsx
@@ -1,32 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAuthMangedHook from "../hook/useAuthMangedHook";
+import usePublicHook from "../Api/usePublicHook";
 
-
-
-import { useQuery } from '@tanstack/react-query'
-import React from 'react'
-import useAuthMangedHook from '../hook/useAuthMangedHook'
-
+import {
+  ResponsiveContainer,
+  ComposedChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Bar,
+  Line,
+} from "recharts";
 function Dashboard() {
+  let { user } = useAuthMangedHook();
+  const useaxiosapi = usePublicHook();
 
-  let {user}=useAuthMangedHook();
+  const { data: data = [], isLoading } = useQuery({
+    queryKey: ["data",user?.email],
+    queryFn: async () => {
+      const res = await useaxiosapi.get(`/manage-post/${user?.email}`);
 
-  const {data:dashBoardData=[],isLoading}=useQuery({
-    queryKey:['data'],
-    queryFn: () =>{
-
-    }
-    
-
-  })
-  
+      return res.data;
+    },
+  });
+  console.log("dashboard", data);
 
   return (
     <div>
-      {/* show all data using grap */}
+      <ComposedChart
+        width={500}
+        height={500}
+        data={data}
+        className="border"
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid />
+        <XAxis dataKey="Date" scale="band" />
+        <YAxis dataKey={"Like"} />
 
-       
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="Like" barSize={20} fill="#413ea0" />
 
+        <Line
+          type="monotone"
+          dataKey="UnLike"
+          stroke="#ff7300"
+        />
+      </ComposedChart>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
